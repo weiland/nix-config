874a205ee29ff0fd4a76288e2bc8a5b7f256e553
Pascal Weiland <weiland@users.noreply.github.com>
2024-03-15 18:29:44 +0100

    feat: create user config

--- hosts/Hopper/default.nix
+++ hosts/Hopper/default.nix
@@ -3,27 +3,16 @@
   imports = [
     ../../modules/darwin
     {
-      networking.computerName = "Hopper MBP";
+      networking.computerName = "Hopper";
       networking.hostName = "Hopper";
       networking.localHostName = "Hopper";
       system.defaults.smb.NetBIOSName = "Hopper";
     }
+    ../users/pw
   ];
-
-  users.users.pw = {
-    name = "pw";
-    home = "/Users/pw";
-  };
 
   # home-manager
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.users.pw.imports = [
-    ../../modules/home
-    {
-      home.stateVersion = "23.11";
-      home.username = "pw";
-    }
-  ];
 
 }
---
+++ hosts/users/pw/default.nix
@@ -0,0 +1,16 @@
+{ ... } : {
+
+  users.users.pw = {
+    name = "pw";
+    home = "/Users/pw";
+  };
+
+  home-manager.users.pw.imports = [
+    ../../../modules/home
+    {
+      home.stateVersion = "23.11";
+      home.username = "pw";
+    }
+  ];
+
+}
