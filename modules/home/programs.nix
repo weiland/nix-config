609d452f2fde49cf12db063be83f5d3d2a4619a7
Pascal Weiland <weiland@users.noreply.github.com>
2025-07-25 11:17:29 +0200

    fish: remove some default colors

--- modules/home/programs.nix
+++ modules/home/programs.nix
@@ -64,23 +64,23 @@
       # '';
       shellInit = ''
         # Set syntax highlighting colours; var names defined here:
-        set fish_color_normal normal
-        set fish_color_command white
-        set fish_color_quote brgreen
-        set fish_color_redirection brblue
-        set fish_color_end white
-        set fish_color_error -o brred
-        set fish_color_param brpurple
-        set fish_color_comment --italics brblack
-        set fish_color_match cyan
-        set fish_color_search_match --background=brblack
-        set fish_color_operator cyan
-        set fish_color_escape white
-        set fish_color_autosuggestion brblack
+        #set fish_color_normal normal
+        #set fish_color_command white
+        #set fish_color_quote brgreen
+        #set fish_color_redirection brblue
+        #set fish_color_end white
+        #set fish_color_error -o brred
+        #set fish_color_param brpurple
+        #set fish_color_comment --italics brblack
+        #set fish_color_match cyan
+        #set fish_color_search_match --background=brblack
+        #set fish_color_operator cyan
+        #set fish_color_escape white
+        #set fish_color_autosuggestion brblack
 
         any-nix-shell fish --info-right | source
 
-        # fix ghostty
+        # fix done command for ghostty
         if test "$TERM_PROGRAM" = "ghostty"
           set __done_notification_command "echo -e \"\033]777;notify;\$title;\$message\a\""
         end
