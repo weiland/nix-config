5d2f176d337e5f51a419df225fff75c7f4b07107
Pascal Weiland <weiland@users.noreply.github.com>
2025-07-27 13:11:45 +0200

    upgrade home-manager

--- hosts/users/pw/default.nix
+++ hosts/users/pw/default.nix
@@ -12,7 +12,7 @@ in
   home-manager.users.${username}.imports = [
     ../../../modules/home
     {
-      home.stateVersion = "23.11";
+      home.stateVersion = "25.05";
       home.username = username;
     }
   ];
