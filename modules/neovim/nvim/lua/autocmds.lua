7498d73cc295c3d37573dee2f31ac7c58640e54c
Pascal Weiland <weiland@users.noreply.github.com>
2025-08-14 18:11:45 +0200

    nvim: remove unused code

--- modules/neovim/nvim/lua/autocmds.lua
+++ modules/neovim/nvim/lua/autocmds.lua
@@ -3,19 +3,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function() vim.highlight.on_yank() end,
     desc = "Briefly highlight selection on yank."
 })
-
--- Return to last edit position when opening files (instead of relativenumbers)
--- local api = vim.api
--- local cmd = vim.cmd
--- api.nvim_create_autocmd('BufReadPost', {
---     group = vim.g.user.event,
---     callback = function(args)
---         local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
---         local not_commit = vim.b[args.buf].filetype ~= 'commit'
-
---         if valid_line and not_commit then
---             cmd([[normal! g`"]])
---         end
---     end,
---     desc = "Jump to the last known and valid cursor position (when not in commit)."
--- })
