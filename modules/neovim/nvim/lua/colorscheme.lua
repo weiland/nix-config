6956f6a76f137da3e60d696ac1348ff22b1c00ce
Pascal Weiland <weiland@users.noreply.github.com>
2025-08-14 14:40:22 +0200

    nvim: use catppuccin
    rose-pine orange text was too light

--- modules/neovim/nvim/lua/colorscheme.lua
+++ modules/neovim/nvim/lua/colorscheme.lua
@@ -1,7 +1,6 @@
--- require('ayu').colorscheme()
--- vim.cmd 'colorscheme onenord'
-require("rose-pine").setup({
-    variant = "dawn", -- auto, main, moon, or dawn
-    dark_variant = "moon", -- main, moon, or dawn
-})
-vim.cmd("colorscheme rose-pine")
+-- require("ayu").colorscheme()
+-- require("rose-pine").setup({
+--     variant = "dawn", -- auto, main, moon, or dawn
+--     dark_variant = "moon", -- main, moon, or dawn
+-- })
+vim.cmd("colorscheme catppuccin")
