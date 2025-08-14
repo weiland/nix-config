9598167855376ce3fc7a824de5133db9a573ec2c
Pascal Weiland <weiland@users.noreply.github.com>
2025-08-14 18:10:20 +0200

    nvim: attempt to fix last cursor position in gitcommit

--- modules/neovim/nvim/lua/autocmds.lua
+++ modules/neovim/nvim/lua/autocmds.lua
@@ -1,26 +1,21 @@
-local cmd = vim.cmd
-
 -- Highlight selection on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function() vim.highlight.on_yank() end,
-  desc = "Briefly highlight selection on yank."
+    callback = function() vim.highlight.on_yank() end,
+    desc = "Briefly highlight selection on yank."
 })
 
 -- Return to last edit position when opening files (instead of relativenumbers)
--- autocmd BufReadPost *
---      \ if line("'\"") > 0 && &filetype != "gitcommit" && line("'\"") <= line("$") |
---      \   exe "normal! g`\"" |
---      \ endif
+-- local api = vim.api
+-- local cmd = vim.cmd
+-- api.nvim_create_autocmd('BufReadPost', {
+--     group = vim.g.user.event,
+--     callback = function(args)
+--         local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+--         local not_commit = vim.b[args.buf].filetype ~= 'commit'
 
-local api = vim.api
-api.nvim_create_autocmd({ 'BufRead', 'BufReadPost' }, {
-  callback = function()
-    local row, column = unpack(api.nvim_buf_get_mark(0, '"'))
-    local buf_line_count = api.nvim_buf_line_count(0)
-
-    if row >= 1 and row <= buf_line_count and vim.bo.filetype ~= "gitcommit" then
-      api.nvim_win_set_cursor(0, { row, column })
-    end
-  end,
-  desc = "Remember last cursor position, except for git commits."
-})
+--         if valid_line and not_commit then
+--             cmd([[normal! g`"]])
+--         end
+--     end,
+--     desc = "Jump to the last known and valid cursor position (when not in commit)."
+-- })
