35de31132e02b2e4543b35e861b642c477ef6c06
Pascal Weiland <weiland@users.noreply.github.com>
2025-08-14 16:10:32 +0200

    nix: manage nix via nix-darwin

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   nix.settings = {
     experimental-features = "nix-command flakes";
   };
@@ -7,4 +7,15 @@
     allowUnfree = true;
     input-fonts.acceptLicense = true;
   };
+
+  # Automatically run the nix store optimiser at a specific time.
+  #nix.optimise.automatic = true;
+  #nix.gc = {
+  #  automatic = true;
+  #  options = "--delete-older-than 14d";
+  #};
+
+  # manage nix with nix-darwin
+  nix.enable = true;
+  nix.package = pkgs.nix;
 }
--- modules/home/programs/shell.nix
+++ modules/home/programs/shell.nix
@@ -1,19 +1,15 @@
-{ pkgs, ... }:
-let
+{pkgs, ...}: let
   catppuccin-fish = pkgs.fetchFromGitHub {
     owner = "catppuccin";
     repo = "fish";
     rev = "6a85af2ff722ad0f9fbc8424ea0a5c454661dfed";
     hash = "sha256-Oc0emnIUI4LV7QJLs4B2/FQtCFewRFVp7EDv8GawFsA=";
   };
-in
-{
+in {
   # fish themes
 
-  xdg.configFile."fish/themes/Catppuccin Latte.theme".source =
-    "${catppuccin-fish}/themes/Catppuccin Latte.theme";
-  xdg.configFile."fish/themes/Catppuccin Macchiato.theme".source =
-    "${catppuccin-fish}/themes/Catppuccin Macchiato.theme";
+  xdg.configFile."fish/themes/Catppuccin Latte.theme".source = "${catppuccin-fish}/themes/Catppuccin Latte.theme";
+  xdg.configFile."fish/themes/Catppuccin Macchiato.theme".source = "${catppuccin-fish}/themes/Catppuccin Macchiato.theme";
 
   home.packages = [
     pkgs.fishPlugins.done
