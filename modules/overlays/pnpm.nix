8bc033471b533dae6e398fbc7ac5b09df78577c0
Pascal Weiland <weiland@users.noreply.github.com>
2025-09-02 10:22:47 +0200

    fix: pnpm hash

--- modules/overlays/pnpm.nix
+++ modules/overlays/pnpm.nix
@@ -3,7 +3,7 @@ final: prev: {
     version = "10.15.1";
     src = prev.fetchurl {
       url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
-      hash = "sha256-hMGeeI19fuJI5Ka3FS+Ou6D0/nOApfRDyhfXbAMAUtI=";
+      hash = "sha256-jFOvAq4+wfsK51N3+NTWIXwtfL5vA8FjUMq/dJPebv8=";
     };
   });
 }
