f2af408b0c8fa1a768d24f0d07860c5671ecbd2e
Pascal Weiland <weiland@users.noreply.github.com>
2025-09-13 11:32:58 +0200

    deps(overlay): upgrade pnpm to v10.16.0

--- modules/overlays/pnpm.nix
+++ modules/overlays/pnpm.nix
@@ -1,9 +1,9 @@
 final: prev: {
   pnpm = prev.pnpm.overrideAttrs (oldAttrs: rec {
-    version = "10.15.1";
+    version = "10.16.0";
     src = prev.fetchurl {
       url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
-      hash = "sha256-jFOvAq4+wfsK51N3+NTWIXwtfL5vA8FjUMq/dJPebv8=";
+      hash = "sha256-3WLTjfGmcCyTD7oUuSvDyj1cJKbHAFsuU1Wpi4mPpdQ=";
     };
   });
 }
