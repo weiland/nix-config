e9d42d234835b180cfacc285f1515a5f73fea06a
Pascal Weiland <weiland@users.noreply.github.com>
2025-09-17 21:19:47 +0200

    overlay: update pnpm and use nixpkgs nodejs

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -2,7 +2,7 @@
 {
   nixpkgs.overlays = [
     (import ./bun.nix)
-    (import ./nodejs_24.nix)
+    # (import ./nodejs_24.nix)
     (import ./pnpm.nix)
   ];
 }
--- modules/overlays/pnpm.nix
+++ modules/overlays/pnpm.nix
@@ -1,9 +1,9 @@
 final: prev: {
   pnpm = prev.pnpm.overrideAttrs (oldAttrs: rec {
-    version = "10.16.1";
+    version = "10.17.0";
     src = prev.fetchurl {
       url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
-      hash = "sha256-t36Sug1ZpjcrbFBBu7P4ZvuF6SffMzgn8Mf1d8XhpxM=";
+      hash = "sha256-vZ7FQXZBOR4KyzkSspEr1bA4VAeoLalHRKdAe1Z/208=";
     };
   });
 }
