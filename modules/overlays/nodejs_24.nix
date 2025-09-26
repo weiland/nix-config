e4b48448e420490019a70c849e1cb07834522505
Pascal Weiland <weiland@users.noreply.github.com>
2025-09-26 16:46:16 +0200

    overlay: upgrade nodejs to v24.9.0

--- modules/overlays/nodejs_24.nix
+++ modules/overlays/nodejs_24.nix
@@ -2,10 +2,10 @@ final: prev: {
   # this worked (using the same package as nixpkgs do)
   # takes a long time (like using the GitHub code)
   nodejs_24 = prev.nodejs_24.overrideAttrs (oldAttrs: rec {
-    version = "24.8.0";
+    version = "24.9.0";
     src = prev.fetchurl {
       url = "https://nodejs.org/dist/v${version}/node-v${version}.tar.xz";
-      hash = "sha256-HAOzYuv0dA1HWLmj0wh+PemJ9UgjZQ7IC0cJDvQUsuA=";
+      hash = "sha256-8XvEywH1kJjDSiiMG7EJp3iGfBTusOu9YI0GF7EZO78=";
     };
   });
 }
