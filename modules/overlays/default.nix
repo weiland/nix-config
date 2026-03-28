3207e65e8874295b4303f8daae0a9e2c86ffbb1e
Pascal Weiland <weiland@users.noreply.github.com>
2026-03-28 22:04:50 +0100

    feat: remove direnv overlay
    since the package is fixed again on macos

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -1,17 +1,4 @@
-{ inputs, ... }:
+{ ... }:
 {
-  nixpkgs.overlays = [
-    (
-      final: prev:
-      let
-        masterPkgs = import inputs.nixpkgs-master {
-          inherit (final.stdenv) system;
-          config.allowUnfree = true;
-        };
-      in
-      {
-        direnv = masterPkgs.direnv;
-      }
-    )
-  ];
+  nixpkgs.overlays = [ ];
 }
