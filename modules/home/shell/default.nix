460b84b5bc5c5021904aeb30730fa1b727c8efce
Pascal Weiland <weiland@users.noreply.github.com>
2026-05-04 12:17:31 +0200

    fix: disable direnv fix

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -34,12 +34,6 @@
     direnv = {
       enable = true;
       nix-direnv.enable = true;
-      package = pkgs.direnv.overrideAttrs (oldAttrs: {
-        env = (oldAttrs.env or { }) // {
-          CGO_ENABLED = "1";
-        };
-        doCheck = false;
-      });
     };
   };
 }
