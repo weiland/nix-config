4cb7a2a9905a9bcce99bc84b3310f4b51b90e02c
Pascal Weiland <weiland@users.noreply.github.com>
2026-07-06 12:27:01 +0200

    fix(darwin): disable docs for nix-darwin

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -6,6 +6,10 @@
     ../common/nix.nix
   ];
 
+  # FIXME: remove when resolved https://github.com/nix-darwin/nix-darwin/issues/1817#issuecomment-4887465960
+  documentation.enable = false;
+  system.tools.darwin-uninstaller.enable = false;
+
   time.timeZone = "Europe/Berlin";
 
   programs.zsh.enable = true;
