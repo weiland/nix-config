09e5d6480da849958da835226803ea7ea5dfc32d
Pascal Weiland <weiland@users.noreply.github.com>
2026-08-20 12:33:38 +0200

    feat: add yt-dlp to home-manager
    but still deactivated until package is fixed and running
    `nix run nixpkgs#yt-dlp` succeeds.

--- modules/home/programs/other-programs.nix
+++ modules/home/programs/other-programs.nix
@@ -49,6 +49,21 @@
       ];
     };
 
+    yt-dlp = {
+      enable = false;
+      extraConfig = ''
+        --output ~/Downloads/Videos/%(webpage_url_domain)s--%(playlist|no-playlist)s--%(title)s--%(id)s.%(ext)s
+
+        # Use only ASCII characters, and avoid "&" and spaces in filenames
+        --restrict-filenames
+
+        # --download-archive ~/Downloads/Videos/ytdlp-already-downloaded-videos.txt
+
+        # mark watched (even with --simulate) NOTE: can be annoying when playing around
+        --mark-watched
+      '';
+    };
+
     zoxide = {
       enable = true;
       enableFishIntegration = true;
